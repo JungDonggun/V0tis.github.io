abbcf0eab01344d20a5592d98eaa7bdf151c5212
JungDonggun <donggun.dev@gmail.com>
2022-04-08 12:05:09 +0900

    add title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 // Modules
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head>
-          {/*<meta name="theme-color" content={theme.palette.primary.main} />*/}
-          <link rel="stylesheet"
-                href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap"/>
-        </Head>
-        <body>
-        <Main/>
-        <NextScript/>
-        </body>
-      </Html>
+        <Html lang="en">
+          <Head>
+            {/*<meta name="theme-color" content={theme.palette.primary.main} />*/}
+            <title>동건이에요~</title>
+            <link rel="stylesheet"
+                  href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap"/>
+          </Head>
+          <body>
+          <Main/>
+          <NextScript/>
+          </body>
+        </Html>
     );
   }
 }
